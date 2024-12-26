70c7739228e971c347b9a820edf01465d9c22d82
Elizabeth <lizagotovitryby@gmail.com>
2024-12-26 09:40:49 -0500

    first typescript small app

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import { concatenation } from './concat';
+
+const button = document.querySelector('button')!;
+const input = document.querySelector('input')!;
+
+if (button && input) {
+  button.addEventListener('click', () => {
+    concatenation(input.value, 'hello!');
+  });
+}
