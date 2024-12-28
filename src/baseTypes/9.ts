e0bd0c25448755246b79c367856ec52d91a45b6b
Elizabeth <lizagotovitryby@gmail.com>
2024-12-28 17:53:24 -0500

    solved Task 9

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,8 +1,38 @@
 /*
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
+// const page1 = {
+//   title: 'The awesome page',
+//   likes: 100,
+//   accounts: ['Max', 'Anton', 'Nikita'],
+//   status: 'open',
+//   details: {
+//     createAt: new Date('2021-01-01'),
+//     updateAt: new Date('2021-05-01'),
+//   }
+// }
 
-const page1 = {
+// const page2 = {
+//   title: 'Python or Js',
+//   likes: 5,
+//   accounts: ['Alex'],
+//   status: 'close',
+// }
+
+// Solution
+type Details = {
+  createAt: Date;
+  updateAt: Date;
+}
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: string;
+  details: Details;
+}
+
+const page1: Page = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -13,7 +43,7 @@ const page1 = {
   }
 }
 
-const page2 = {
+const page2: Partial<Page> = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
