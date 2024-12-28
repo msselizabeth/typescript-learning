a0b93a2318d2f8cd5edc71da31eb2267ed063c22
Elizabeth <lizagotovitryby@gmail.com>
2024-12-28 17:05:15 -0500

    Added tasks for types

--- dist/index.js
+++
@@ -1,8 +0,0 @@
-import { concatenation } from './concat';
-const button = document.querySelector('button');
-const input = document.querySelector('input');
-if (button && input) {
-    button.addEventListener('click', () => {
-        concatenation(input.value, 'hello!');
-    });
-}
---
+++ src/baseTypes/1.ts
@@ -0,0 +1,24 @@
+// Convert this code to TypeScript by specifying the appropriate types for all variables.
+
+// let age = 50;
+let age: number = 50;
+
+// let name = 'Max';
+let name: string = "Max";
+
+// let toggle = true;
+let toggle: boolean = true;
+
+// let empty = null;
+let empty: null = null;
+
+// let notInitialize;
+let notInitialize: unknown;
+
+// let callback = (a) => { return 100 + a }; 
+let callback = (a: number): number => {
+    return 100 + a;
+}
+
+
+export {};
