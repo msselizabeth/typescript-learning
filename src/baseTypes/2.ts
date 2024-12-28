27856a0d3f90e73362df76104da271d3195ee3b0
Elizabeth <lizagotovitryby@gmail.com>
2024-12-28 17:08:07 -0500

    solved 2nd task

--- src/baseTypes/2.ts
+++ src/baseTypes/2.ts
@@ -1,4 +1,12 @@
-let anything = -20;
+// A JavaScript variable can store a value of any type:
+
+// let anything = -20;
+// anything = 'Text';
+// anything = {};
+
+// What type do you give the anything variable in TypeScript to keep it flexible?
+
+let anything:any = -20;
 anything = 'Text';
 anything = {};
 
