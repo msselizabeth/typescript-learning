bc47f6afb12a1a12f0e07c054800d3dfcd1903ec
Elizabeth <lizagotovitryby@gmail.com>
2024-12-28 17:13:52 -0500

    solved 4 task

--- src/baseTypes/4.ts
+++ src/baseTypes/4.ts
@@ -1,8 +1,9 @@
-/*
-  Як переписати його в TypeScript, використовуючи концепцію кортежів, 
-  щоб гарантувати, що перший елемент завжди буде рядком, а другий числом?
-*/
+// You have the following JavaScript array:
 
-let person = ['Max', 21];
+// let person = ['Max', 21];
+// How can you rewrite it in TypeScript using the concept of tuples to ensure that the first element is always a string and the second is a number?
+
+// Solution
+let person: [string, number] = ['Max', 21];
 
 export {};
