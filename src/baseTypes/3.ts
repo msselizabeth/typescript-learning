530e53494dffc756f9accf9253147040da3950aa
Elizabeth <lizagotovitryby@gmail.com>
2024-12-28 17:11:47 -0500

    solved 3rd task

--- src/baseTypes/3.ts
+++ src/baseTypes/3.ts
@@ -1,6 +1,18 @@
-let some:unknown;
-some = 'Text';
+// In TypeScript, the unknown type allows us to store any value, but we can assign an unknown variable directly to another variable if we are sure of its type. You have the following code:
+
+// let some:unknown;
+// some = 'Text';
+// let str: string;
+// str = some;
+// What needs to be fixed in this code to make it correct and safe?
+
+
+// Solution:
+let some: unknown;
 let str: string;
-str = some;
+some = 'Text';
+if (typeof some === "string") {
+    str = some;
+}
 
 export {};
