06778c7863c924190f4325e3f1e6341f93b5af7c
Elizabeth <lizagotovitryby@gmail.com>
2024-12-29 10:35:12 -0500

    solved generic Task 4

--- src/generic/4.ts
+++ src/generic/4.ts
@@ -1,17 +1,24 @@
 /*
-  Використовуйте generics та інтерфейси, щоб виправити помилку в наступних класах:
+ Task:
+ Use generics and interfaces to fix the error in the following classes:
 */
 
-class Component {
+// Solution
+interface PageProps {
+  title: string;
+}
+class Component<T> {
   constructor (public props:T) {
-
   }
 }
 
-class Page extends Component {
+class Page extends Component<PageProps> {
   pageInfo () {
     console.log(this.props.title);
   }
 }
 
+// Example Usage:
+const page = new Page({title: "My title"});
+page.pageInfo();
 export {};
