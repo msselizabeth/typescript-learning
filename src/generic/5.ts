2a00ed3e943e5e26d1d4498f54fea74fa97413aa
Elizabeth <lizagotovitryby@gmail.com>
2024-12-29 11:14:57 -0500

    solved generic Task 5

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -1,11 +1,29 @@
+// Task
 /*
-  Вам потрібно реалізувати інтерфейс KeyValuePair, який описує пару ключ-значення. 
-  Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
+You need to implement the KeyValuePair interface, which describes a key-value pair.
+Use generics so that this interface can work with any type of key and value.
 */
 
-interface KeyValuePair {
-  key;
-  value;
+// Solution
+interface KeyValuePair<T, U> {
+  key: T;
+  value: U;
 }
+
+// Example usage
+function createKeyValuePair<T, U>(key: T, value: U): KeyValuePair<T, U> {
+  return { key, value };
+}
+const pair1 = createKeyValuePair("name", "Alice"); 
+const pair2 = createKeyValuePair("age", 35); 
+
+console.log(pair1);
+console.log(pair2);
+
+const resultObject = {
+  [pair1.key]: pair1.value,
+  [pair2.key]: pair2.value,
+};
+console.log(resultObject);
 
 export {};
