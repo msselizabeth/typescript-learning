1fc4fc6025f1f663777af9d05cf426d34a823fd3
Elizabeth <lizagotovitryby@gmail.com>
2024-12-29 14:03:28 -0500

    solved classes Task 3

--- ts/src/classes/3.ts
+++ ts/src/classes/3.ts
@@ -1,26 +1,36 @@
-/*
-  Ви створюєте гру, де є персонажі з різними ролями.
-  Зараз ви працюєте над класом Wizard, який має реалізовувати два інтерфейси - ICharacter та ISpellCaster.
-
-  Визначте інтерфейси ICharacter та ISpellCaster так, щоб вони відповідали вимогам класу Wizard. 
-  Інтерфейс ICharacter повинен включати властивості name і level, і навіть метод introduce і levelUp. 
-  Інтерфейс ISpellCaster повинен включати метод castSpell.
+/**
+ * You are creating a game where there are characters with different roles.
+ * You are currently working on a Wizard class that should implement two interfaces, ICharacter and ISpellCaster.
+ * Define the ICharacter and ISpellCaster interfaces so that they meet the requirements of the Wizard class.
+ * The ICharacter interface should include properties name and level, as well as methods introduce and levelUp.
+ * The ISpellCaster interface should include a method castSpell.
 */
 
-// реалізація класу Wizard
+// Solution
+interface ICharacter {
+  name: string;
+  level: number;
+  introduce(phrase: string): void;
+  levelUp(level: number): void;
+}
+
+interface ISpellCaster {
+  castSpell(): void;
+}
+// implementing Wizard class
 class Wizard implements ICharacter, ISpellCaster {
   constructor(public name: string, public level: number) {
     if (this.level < 1) {
-      throw new Error('Level too low');
+      throw new Error("Level too low");
     }
   }
 
   introduce(phrase: string): void {
-    console.log(phrase + ', ' + this.name);
+    console.log(phrase + ", " + this.name);
   }
 
   castSpell(): void {
-    console.log('Casting a spell, behold my power!');
+    console.log("Casting a spell, behold my power!");
   }
 
   levelUp(): void {
@@ -29,11 +39,10 @@ class Wizard implements ICharacter, ISpellCaster {
   }
 }
 
-// тестування класу
-const wizard = new Wizard('Merlin', 15);
+// Example usage
+const wizard = new Wizard("Merlin", 15);
+console.log(wizard);
 
-wizard.introduce('I am the mighty wizard');
+wizard.introduce("I am the mighty wizard");
 wizard.castSpell();
-wizard.levelUp();  // Level up! New level is 16
-
-export {};
+wizard.levelUp(); // Level up! New level is 16
