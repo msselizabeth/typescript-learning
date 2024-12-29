69f78dd19286c710abe0c8ea126fe3f2f6a81bb7
Elizabeth <lizagotovitryby@gmail.com>
2024-12-29 10:03:36 -0500

    solved generic Task 2

--- src/generic/2.ts
+++ src/generic/2.ts
@@ -1,23 +1,27 @@
+// Task
 /*
-  У вас є тип AllType. Існує функція compare, яка приймає два об'єкти. Ці об'єкти містять поля AllType. 
-  Ваше завдання – використовувати Pick та generics для вказівки, що поля цих об'єктів належать AllType.
-  Функція compare повинна повертати AllType.
+  You have a type AllType. There is a compare function that takes two objects. These objects contain AllType fields.
+  Your task is to use Pick and generics to indicate that the fields of these objects belong to AllType.
+  The compare function should return AllType.
 */
 
 type AllType = {
   name: string;
   position: number;
   color: string;
-  weight: number
-}
-
-function compare (top, bottom): AllType {
+  weight: number;
+};
+// Solution
+function compare(
+  top: Pick<AllType, "name" | "color">,
+  bottom: Pick<AllType, "position" | "weight">
+): AllType {
   return {
     name: top.name,
     color: top.color,
     position: bottom.position,
     weight: bottom.weight,
-  }
+  };
 }
 
 export {};
