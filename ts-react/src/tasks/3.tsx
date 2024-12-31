389c7ea4a36aba9722b11ff74eb7abd01b8ab980
Elizabeth <lizagotovitryby@gmail.com>
2024-12-31 11:03:29 -0500

    solved task 3

--- ts-react/src/tasks/3.tsx
+++ ts-react/src/tasks/3.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+// You are creating a form component in React. You have an input field that you want to track changes to. To do this, you use an onChange event handler. Your job is to correctly type the event that is passed to this function.
+
+import React, { ChangeEvent, useState } from 'react';
 
 export function FormComponent() {
-  const [value, setValue] = useState("");
+  const [value, setValue] = useState<string>("");
 
-  const handleChange = (event) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
 
